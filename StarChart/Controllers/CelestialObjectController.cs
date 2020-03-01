b04d8293ef8ac2443d90b03f84c6ab151a158935
Nidhi Gupta <nidhigupta@quickenloans.com>
2020-02-29 22:26:37 -0500

    added GetByName and GetAll.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -21,19 +22,47 @@ namespace StarChart.Controllers
         public IActionResult GetById(int id)
         {
             var celestialObject = _context.CelestialObjects?.FirstOrDefault(c => c.Id.Equals(id));
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+            AddSatellites(celestialObject);
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects?.FirstOrDefault(c => c.Name.Equals(name));
 
             if (celestialObject == null)
             {
                 return NotFound();
             }
+            AddSatellites(celestialObject);
+            return Ok(celestialObject);
+        }
 
-            var satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId != null && c.OrbitedObjectId.Equals(id));
+        [HttpGet()]
+        public IActionResult GetAll()
+        {
+            if (_context.CelestialObjects == null || !_context.CelestialObjects.Any())
+                return NotFound();
 
+            foreach (var celestialObject in _context.CelestialObjects)
+            {
+                AddSatellites(celestialObject);
+            }
+            return Ok(_context.CelestialObjects);
+        }
+
+        private void AddSatellites(CelestialObject celestialObject)
+        {
+            var satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId != null && c.OrbitedObjectId.Equals(celestialObject.Id)).ToList();
             foreach (var satellite in satellites)
             {
                 celestialObject.Satellites.Add(satellite);
             }
-            return Ok(celestialObject);
         }
     }
 }
