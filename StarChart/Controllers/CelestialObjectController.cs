7b5be7383fa80855b4d11db19aa286925412d3de
Nidhi Gupta <nidhigupta@quickenloans.com>
2020-02-29 22:17:24 -0500

    added GetById method.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -9,13 +9,31 @@ namespace StarChart.Controllers
 {
     [ApiController]
     [Route("")]
-    public class CelestialObjectController: ControllerBase
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects?.FirstOrDefault(c => c.Id.Equals(id));
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            var satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId != null && c.OrbitedObjectId.Equals(id));
+
+            foreach (var satellite in satellites)
+            {
+                celestialObject.Satellites.Add(satellite);
+            }
+            return Ok(celestialObject);
         }
     }
 }
