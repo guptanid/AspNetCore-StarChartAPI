22cb49f365a7179787e849ae092ccc5317dca063
Nidhi Gupta <nidhigupta@quickenloans.com>
2020-02-29 21:45:21 -0500

    added bew API Controller

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class CelestialObjectController: ControllerBase
+    {
+        private ApplicationDbContext _context { get; set; }
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
